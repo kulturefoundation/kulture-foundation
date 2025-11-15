e6b4664051bed3ea77b4fb872dc08f9cdcdf9aa2
kulturefoundation <ssebunyaedgar@gmail.com>
2025-11-15 15:20:20 +0300

    Add Next.js project structure

---
+++ next.config.js
@@ -0,0 +1,41 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+       {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+       {
+        protocol: 'https' ,
+        hostname: 'encrypted-tbn0.gstatic.com',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
