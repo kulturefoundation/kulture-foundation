9b6b4777a3c5579ec4f44a9af694ae9cdf2acc0a
Kulture Foundation <ssebunyaedgar@gmail.com>
2026-03-06 20:52:01 +0300

    Updated project ID to studio-408998664-6176b

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,8 @@
-
 export const firebaseConfig = {
-  projectId: 'kulture-foundation-45074',
-  appId: '1:717605313065:web:d81377a8c89e82cc66da38',
-  storageBucket: 'kulture-foundation-45074.appspot.com',
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: 'kulture-foundation-45074.firebaseapp.com',
-  messagingSenderId: '717605313065',
-  measurementId: 'G-58K6BP01Q1',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
